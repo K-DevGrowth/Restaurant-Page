79e2f618da902bd5a3165c28f7bb996a089bc401
Khoi <khoidang01234@gmail.com>
2025-03-08 13:16:19 +0700

    Separate webpack.config to 3 webpacks

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -21,6 +16,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
                 test: /\.html$/i,
                 loader: "html-loader",
             },
@@ -28,10 +27,7 @@ module.exports = {
                 test: /\.png$/i,
                 type: "asset/resource",
             },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            }
+            
         ],
     },
 }
